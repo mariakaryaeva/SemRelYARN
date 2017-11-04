1918ce0b9edbb86d96315d4fa3d8d27919279228
mariakaryaeva <mari.karyaeva@gmail.com>
2017-11-04 14:16:33 +0300

    Close issue 5 (clear wikionary pairs using freq list)

--- similarity.py
+++ similarity.py
@@ -8,7 +8,7 @@ freq_list = []
 freq = freq_file.readlines()
 
 count = 0
-while(count< 40000):
+while(count< 60000):
     line = freq[count]
     freq_list.append(re.split("\t",line)[0])
     count = count + 1
@@ -16,16 +16,18 @@ while(count< 40000):
 
 wictionary_file = open("wikionary_hypernyms.txt", "r")
 
-out_file = open("wikionary_clear_40k_freq.txt", "w")
+out_file = open("wikionary_clear_60k_freq.txt", "w")
 for line in wictionary_file:
     word1 = re.split("#",line)[0].lower()
     word2 = re.sub("\n", "", re.split("#", line)[1].lower())
     if word1 in freq_list and word2 in freq_list:
         out_file.write(line)
+out_file.close()
 
-out_file = open("wikionary_clear_40k_rod_freq.txt", "w")
+out_r_file = open("wikionary_clear_60k_rod_freq.txt", "w")
 for line in wictionary_file:
     word1 = re.split("#",line)[0].lower()
     word2 = re.sub("\n", "", re.split("#", line)[1].lower())
     if word1 in freq_list and word1!="имя":
-        out_file.write(line)
+        out_r_file.write(line)
+out_r_file.close()
